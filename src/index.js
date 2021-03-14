6e0f1d85fc092f4636157b7e7851d3dd7af98974
Grigory Taran <grunder-96@mail.ru>
2021-03-14 18:15:37 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+  
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let strPair = bracketsConfig.map(function mass(currentValue) {
+    return currentValue.join("");
+  });
+  let strCheck = str;
+
+  for (let i = 0; i < strPair.length; i++) {
+      if (str.includes(strPair[i])) {
+          strCheck = str.replace(strPair[i], "");
+          i = -1;
+      }
+      str = strCheck;
+  }
+
+  if (strCheck === "") {
+    return true;
+  } else {
+    return false;
+  }
+};
